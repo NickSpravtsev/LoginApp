fe604a681184604d681617bd8622500278356ddb
Nikolay Spravtsev <nicksprv@icloud.com>
2022-08-22 21:23:44 +0300

    Username text field added

--- LoginApp/Sources/ViewController.swift
+++ LoginApp/Sources/ViewController.swift
@@ -25,10 +25,26 @@ class ViewController: UIViewController {
         label.text = "Login"
         label.textColor = .white
         label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 35)
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
         label.backgroundColor = .white.withAlphaComponent(0)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+
+    private lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+        let icon = UIImage(systemName: "person.circle")
+        textField.setLeftIcon(icon)
+        textField.placeholder = "Username"
+        textField.clearButtonMode = .always
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 20
+        textField.keyboardType = .emailAddress
+        textField.returnKeyType = .next
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
     }()
 
     // MARK: - Lifecycle
@@ -44,11 +60,13 @@ class ViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(backgroundImageView)
         view.addSubview(loginLabel)
+        view.addSubview(usernameTextField)
     }
 
     private func setupLayout() {
         setupBackgroundImageView()
         setupLoginLabel()
+        setupUsernameTextField()
     }
 
     private func setupBackgroundImageView() {
@@ -68,9 +86,32 @@ class ViewController: UIViewController {
         }
     }
 
+    private func setupUsernameTextField() {
+        usernameTextField.snp.makeConstraints { make in
+            make.top.equalTo(loginLabel.snp.bottom).offset(50)
+            make.left.equalTo(view).offset(40)
+            make.right.equalTo(view).offset(-40)
+            make.height.equalTo(40)
+        }
+    }
+
     // MARK: Actions
 
 
 
+}
+
+extension UITextField {
+    func setLeftIcon(_ image: UIImage?) {
+        if let icon = image {
+            let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+            iconView.tintColor = .black
+            iconView.image = icon
+            let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 40, height: 30))
+            iconContainerView.addSubview(iconView)
+            leftView = iconContainerView
+            leftViewMode = .always
+        }
+    }
 }
 
