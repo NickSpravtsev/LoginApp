9f1c1063daba4ebec72b82f5e4aad3ba924b56bd
Nikolay Spravtsev <nicksprv@icloud.com>
2022-08-22 19:57:59 +0300

    background image added

--- LoginApp/Sources/ViewController.swift
+++ LoginApp/Sources/ViewController.swift
@@ -10,8 +10,45 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    private lazy var imageViewBackground: UIImageView = {
+        let imageBackground = UIImage(named: "background")
+        let imageView = UIImageView(image: imageBackground)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
     }
+
+    // MARK: Setup
+
+    private func setupHierarchy() {
+        view.addSubview(imageViewBackground)
+    }
+
+    private func setupLayout() {
+        setupImageViewBackground()
+    }
+
+    private func setupImageViewBackground() {
+        imageViewBackground.snp.makeConstraints { make in
+            make.right.equalTo(view)
+            make.left.equalTo(view)
+            make.top.equalTo(view)
+            make.bottom.equalTo(view)
+        }
+    }
+
+    // MARK: Actions
+
+
+
 }
 
